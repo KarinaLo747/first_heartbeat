b767497bdd0a7810cc9feac43a800e87e9ef28ab
Wilson <wilsonweiwu@gmail.com>
2024-06-29 23:44:37 +0100

    Created first_heartbeat package and outlined circle analysis main function

---
+++ first_heartbeat/__init__.py
@@ -0,0 +1,9 @@
+__all__ = [
+    'circles_analysis',
+    'utils',
+    'plotter',
+]
+
+import first_heartbeat.circles_analysis
+import first_heartbeat.utils
+import first_heartbeat.plotter
---
+++ first_heartbeat/circles_analysis.py
@@ -0,0 +1,23 @@
+def main() -> None:
+
+    load_data()
+
+    norm_data()
+
+    frame_to_sec()
+
+    plot_fluoresence()
+
+    peak_finding()
+
+    calc_beat_freq()
+
+    get_upbeat()
+
+    calc_t_half()
+
+    calc_direction()
+
+    calc_phase_diff()
+
+    to_csv()
