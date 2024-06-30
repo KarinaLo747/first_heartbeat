f8efaaf6ad6b1a40c427c420c64817638473dac4
Wilson <wilsonweiwu@gmail.com>
2024-06-30 21:27:43 +0100

    Function that implements scipy.signal.find_peaks added.

--- first_heartbeat/circles_analysis.py
+++ first_heartbeat/circles_analysis.py
@@ -1,7 +1,35 @@
+import pandas
+import numpy
+import numpy as np
+from scipy.signal import find_peaks, peak_widths
+from scipy.interpolate import interp1d
 from first_heartbeat.load_data import load_circles
-from first_heartbeat.utils import create_output_dir, norm_df, get_exp_info
+from first_heartbeat.utils import create_output_dir, norm_df, get_exp_info, real_time
 from first_heartbeat.constants import circle_roi_cols
 from first_heartbeat.plotter import time_vs_fluoresence
+
+
+def peak_find(y_data: any, prominence: float = 0.5, height: float = None, width: float = None) -> numpy.ndarray:
+    """Returns the index of peaks found from an array-like input. Uses the scipy.signal.find_peaks.
+
+    Args:
+        y_data (any): Array-like input containing possible peaks to be found, such as fluoresence intensity data containing signals.
+        prominence (float, optional): As described by scipy.signal.find_peaks documentation. Defaults to 0.5.
+        height (float, optional): As described by scipy.signal.find_peaks documentation. Defaults to None.
+        width (float, optional): As described by scipy.signal.find_peaks documentation. Defaults to None.
+
+    Returns:
+        numpy.ndarray: Index of peaks found.
+    """
+
+    peaks, _ = find_peaks(
+        x=y_data,
+        prominence=prominence,
+        height=height,
+        width=width,
+    )
+
+    return peaks
 
 
 def run_analysis(data_dir: str, filter_regex: str = None) -> None:
@@ -42,7 +70,12 @@ def run_analysis(data_dir: str, filter_regex: str = None) -> None:
             xlim=[0, 10],
         )
 
-    # peak_finding()
+    y_data: pandas.Series = norm_data[circle_roi_cols['mean_LL']]
+    peaks_ind: numpy.ndarray = peak_find(
+        y_data=y_data,
+        prominence=0.5,
+    )
+    print(f'{peaks_ind = }')
 
     # calc_beat_freq()
 
