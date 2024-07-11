fb6bf3e046df60d3852e77a8f070acd4db408cee
Wilson <wilsonweiwu@gmail.com>
2024-07-12 00:00:30 +0100

    Only glob dir with CSV files in

--- run_analysis.py
+++ run_analysis.py
@@ -1,16 +1,18 @@
 import glob
+import pathlib
 from first_heartbeat import circles_analysis
 from first_heartbeat.constants import manual_peak_find_csv
 
 
 def main() -> None:
 
-    data_dirs = glob.glob('data/raw/*/*/*/')
-
     with open(manual_peak_find_csv, 'w') as file:
-        pass
+        file.write('dir_path\n')
 
-    for data_dir in data_dirs:
+    csv_files = glob.glob('data/raw/*/*/*/*.csv')
+
+    for csv in csv_files:
+        data_dir = str(pathlib.Path(csv).parent) + '/'
         circles_analysis.run_analysis(data_dir=data_dir, filter_regex='Mean')
 
 
